792cc1e35026aa1334de68d8802357707f38798c
Virginia Cheng <vcheng328@gmail.com>
2023-09-09 01:06:51 -0400

    feat: updated header with tailwind utilities

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,9 @@
-import styles from './Header.module.css'
-
 export default function Header() {
   return (
     <>
-    <header className={styles.header}>
-      <img src="/bao_icon.png" />
+    <header className="w-full h-16 border-b border-b-gray-200 p-4 flex fixed justify-between">
+      <img className="h-full" src="/bao_icon.png" />
+      <button className="rounded-full border border-black px-2 hover:px-3 hover:bg-gray-200 transition-all">Resume</button>
     </header>
   </>
   )
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,30 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Virginia Cheng</title>
         <link rel="icon" href="/bao_icon.png" />
       </Head>
       <Header />
 
-      <main>
-        <h1 className="text-3xl font-bold">
-          Virginia Cheng
-        </h1>
-        <h3>
-          Full-Stack Software Engineer II at EverTrue
-        </h3>
-      </main>
+      <div className="flex mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+          <div className="flex-auto lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+            <div>
+              <h1 className="text-3xl font-bold">
+                Virginia Cheng
+              </h1>
+              <h3>
+                Full-Stack Software Engineer II at EverTrue
+              </h3>
+            </div>
+          </div>
+          <div className="flex-auto pt-24 lg:w-1/2 lg:py-24">
+            Some text about me
+          </div>
+      </div>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
