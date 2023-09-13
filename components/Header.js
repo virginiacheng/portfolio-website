aae9cf507e52f67edcf402e6d3df6e0408812690
Virginia Cheng <vcheng328@gmail.com>
2023-09-13 01:04:41 -0400

    feat: added resume to website

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     <header className="w-full h-16 border-b border-b-slate-400 p-4 flex fixed justify-between bg-slate-900 ">
       <img className="h-full" src="/bao_icon.png" />
       {/* TODO: change colors of button and hover states */}
-      <button className="rounded-full border border-slate-400 px-2 hover:px-3 hover:bg-indigo-200 hover:text-indigo-400 transition-all">Resume</button>
+      <a className="rounded-full border border-slate-400 px-2 hover:px-3 hover:bg-indigo-600 hover:text-indigo-200 hover:text-xl transition-all" href="/Virginia_Cheng_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
     </header>
   </>
   )
