6914f547bfc814b94f8ea4973031197c1bd3e0a6
Virginia Cheng <vcheng328@gmail.com>
2024-02-25 16:45:12 -0500

    fix: spacing and z index bugs

--- components/sections/AboutMe.js
+++ components/sections/AboutMe.js
@@ -7,7 +7,7 @@ export default function AboutMe() {
         className="block h-48 invisible -mt-48"
       ></span>
       <section id="about-content" className="mb-16" data-section>
-        <h2 className="text-3xl font-bold text-indigo-800 mb-4 sticky top-0 bg-slate-300 py-3 z-10 lg:relative">
+        <h2 className="text-3xl font-bold text-indigo-800 sticky top-0 bg-slate-300 py-3 z-10 lg:relative">
           About Me
         </h2>
         <div>
--- components/sections/Experience.js
+++ components/sections/Experience.js
@@ -10,7 +10,7 @@ export default function Experience() {
         className="block h-48 invisible -mt-48"
       ></span>
       <section id="experience-content" className="mb-16" data-section>
-        <h2 className="text-3xl font-bold text-indigo-800 mb-4 sticky top-0 bg-slate-300 py-3 z-10 lg:relative">
+        <h2 className="text-3xl font-bold text-indigo-800 sticky top-0 bg-slate-300 py-3 z-10 lg:relative">
           Experience
         </h2>
         {data.map((item, i) => (
